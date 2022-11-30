821087a8ac9b947f3617a97e32cf92da251cbccc
Phong Tran <vinhphong.tran@gotit.vn>
2022-11-30 17:06:16 +0700

    use state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import Product from './component/Product';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let stateValue;
+
+function useState(intialState) {
+  if (stateValue === undefined) {
+    stateValue = intialState;
+  }
+
+  function setValue(value) {
+    stateValue = value;
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+
+  }
+
+  return [
+    stateValue,
+    setValue
+  ]
+}
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = function() {
+    setCounter(counter + 1);
+  }
+  const handleDecrement = function() {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <Product/>
+      <Product />
+      <h1>Counter { counter }</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
 
 export default App;
+
+// tạo state để quản lý Counter
+// Khi state thay đổi, thì giao diện thay đổi 
