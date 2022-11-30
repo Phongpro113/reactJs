2fd079982658dd93e6313cb11d4665a76620ae7b
Phong Tran <vinhphong.tran@gotit.vn>
2022-11-30 16:02:25 +0700

    create component

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,5 +1,13 @@
+import React from "react";
+
 function Product() {
-    return <h1>Product</h1>;
+    React.createElement('h1')
+    return (
+        <React.Fragment>
+            <h1>Product</h1>
+            <button>create product</button>
+        </React.Fragment>
+    );
 }
 
 export default Product;
